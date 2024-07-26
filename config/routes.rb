cbf93c35397d538e3f0121182ce4db63164d0c88
anhthai <nguyen.doan.anh.thai@sun-asterisk.com>
2024-07-26 13:07:41 +0700

    base layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope "(:locale)", locale: /jp|vi/ do
+    root "static_pages#index"
+    get "/", to: "static_pages#index"
+    get "/register",  to: "users#new"
+    get "/login", to: "sessions#new"
+    resources :users
+  end
 end
